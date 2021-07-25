2cfd83680cf3b882e727fee052969c7c0311a687
Pierre Poupin <pierrep@bam.tech>
2021-07-25 10:51:21 +0200

    adjust formatters settings

--- lua/formatter-config.lua
+++ lua/formatter-config.lua
@@ -1,20 +1,46 @@
-prettierformatter = {
-    -- prettier
-    function()
-      return {
-        exe = "prettierd",
-        args = {vim.api.nvim_buf_get_name(0)},
-        stdin = true
-      }
-    end
+eslintfix = {
+  -- prettier
+  function()
+    return {
+      exe = "eslint_d",
+      args = {"--stdin", "--fix-to-stdout", "--stdin-filename", vim.api.nvim_buf_get_name(0)},
+      stdin = true
+    }
+  end
 }
 
-require('formatter').setup({
-  logging = false,
-  filetype = {
-    javascript = prettierformatter,
-    javascriptreact = prettierformatter,
-    typescript = prettierformatter,
-    typescriptreact = prettierformatter,
+luafmt = {
+  -- luafmt
+  function()
+    return {
+      exe = "luafmt",
+      args = {"--indent-count", 2, "--stdin"},
+      stdin = true
+    }
+  end
+}
+
+prettierjson = {
+  -- prettier
+  function()
+    return {
+      exe = "prettier",
+      args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "false", "--tab-width", "2", "--trailing-commas", "false"},
+      stdin = true
+    }
+  end
+}
+
+require("formatter").setup(
+  {
+    logging = false,
+    filetype = {
+      javascript = eslintfix,
+      javascriptreact = eslintfix,
+      typescript = eslintfix,
+      typescriptreact = eslintfix,
+      json = prettierjson,
+      lua = luafmt
+    }
   }
-})
+)
