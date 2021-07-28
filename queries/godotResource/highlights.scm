9ff009eae230b8ff43e3570ef1eaf51a98520e5f
Pierre Poupin <pierrep@bam.tech>
2021-07-28 19:28:36 +0200

    add gdresource support

---
+++ queries/godotResource/highlights.scm
@@ -0,0 +1,21 @@
+(identifier) @type.builtin
+
+(attribute (identifier) @property)
+(property (path) @property)
+(constructor) @constructor
+
+(string) @string
+(integer) @number
+(float) @float
+
+(true) @constant.builtin
+(false) @constant.builtin
+
+[
+  "["
+  "]"
+] @tag.delimiter
+
+"=" @operator
+
+(ERROR) @error
