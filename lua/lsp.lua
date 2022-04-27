103ba86b3e8c8f3fcfa3c80e169ce6c98876671a
Pierre Poupin <pierrep@bam.tech>
2022-04-28 01:53:50 +0200

    fix cmp mappings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -59,16 +59,22 @@ cmp.setup {
     { name = "path" },
     { name = 'luasnip' },
   },
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm { select = true },
-  },
+  }),
   completion = {
     completeopt = "menu,menuone,noinsert",
   },
+}
+
+cmp.setup.cmdline {
+  mapping = cmp.mapping.preset.cmdline({
+    -- Your configuration here.
+  })
 }
 
 local lspkind = require "lspkind"
