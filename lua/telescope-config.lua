bced63ef9980c961988f490cb6518a995f236c53
Pierre Poupin <pierrep@bam.tech>
2023-11-22 14:36:59 +0100

    tweak telescope

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -4,5 +4,20 @@ require("telescope").setup {}
 -- load_extension, somewhere after setup function:
 require("telescope").load_extension "fzf"
 
-vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files hidden=true<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>;", ":Telescope buffers<CR>", { noremap = true, silent = true })
+
+-- local function find_git_root()
+--   local handle = io.popen "git rev-parse --show-toplevel 2> /dev/null"
+--   local result = handle:read "*a"
+--   handle:close() -- close process
+--   result = result:gsub("%s+$", "") -- trim trailing whitespace
+--   return result
+-- end
+
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<C-p>",
+--   ":Telescope find_files search_dirs={" .. '"' .. find_git_root() .. '"' .. "}<CR>",
+--   { noremap = true, silent = true }
+-- )
