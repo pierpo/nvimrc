6f54aa5377d8af75cb7ff0d2b19541d0ac745989
Pierre Poupin <pierrep@bam.tech>
2024-11-27 01:06:25 +0100

    dont find hidden files with telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -4,7 +4,7 @@ require("telescope").setup {}
 -- load_extension, somewhere after setup function:
 require("telescope").load_extension "fzf"
 
-vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files hidden=true<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>;", ":Telescope buffers<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>gt", ":Telescope live_grep<CR>", { noremap = true, silent = true })
 
