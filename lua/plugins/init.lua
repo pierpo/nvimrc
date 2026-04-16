a7ecbac2b7a001b1e22df47288215abecf0283e6
Pierre Poupin <pierre.poupin@theodo.com>
2026-04-16 16:05:29 +0200

    add outline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -138,4 +138,16 @@ return {
       "nvimtools/none-ls-extras.nvim",
     },
   },
+
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      -- Your setup opts here
+    },
+  },
 }
